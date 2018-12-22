9c361e90087c2f948706b93b309353192cb89fa8
ryancheung <ryancheung.go@gmail.com>
2018-12-22 11:26:39 +0800

    Clear color

--- Main.cpp
+++ Main.cpp
@@ -51,6 +51,9 @@ int main()
         // -----
         processInput(window);
 
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
